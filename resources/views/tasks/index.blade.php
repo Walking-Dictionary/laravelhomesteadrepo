5436324ca2bae4d473a9fca4abb892fe31f15e7a
Walking-Dictionary <gdande@cytonn.com>
2017-03-04 05:13:22 -0500

    Homestead, artisan CLI, Model-View-Controller, Layouts

---
+++ resources/views/tasks/index.blade.php
@@ -0,0 +1,19 @@
+<html>
+
+    <div>
+        <ul>
+
+        @foreach ($tasks as $task)
+
+            <li><a href= "/tasks/{{$task->id}}">
+
+                    {{$task->body}}
+
+                </a>
+            </li>
+        @endforeach
+
+        </ul>
+    </div>
+
+</html>
---
+++ routes/web.php
@@ -0,0 +1,46 @@
+<?php
+
+use App\Task;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+
+
+Route::get('/', function () {
+    $action =>'succeed';
+        $duration => 'everyday';
+
+    return view('welcome', compact('action','duration'));
+
+});
+*/
+
+Route::get('/tasks', function () {
+    //$tasks = DB::table('tasks')->latest()->get(); - without model
+    $tasks = Task::All();
+
+    return view('tasks.index', compact('tasks'));
+});
+
+Route::get('/tasks/{id}', function ($id) {
+
+    //$task = DB::table('tasks')->find($id);
+    $task = Task::find($id);
+
+    return view('tasks.show', compact('task'));
+});
+
+Route::get('about', function (){
+    return view('about');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index');
